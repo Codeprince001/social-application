fa7b3526151c7935c12926036811a9fbd9e43cc4
Wisdom <urangyorkwisdom@gmail.com>
2025-10-01 16:14:00 +0100

    feat: create notification schema

---
+++ backend/src/models/notification.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+    {
+        from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        type: { type: String, enum: ['like', 'comment', 'follow'], required: true },
+        post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
+        isRead: { type: Boolean, default: false },
+        comment: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment'},
+        like: { type: mongoose.Schema.Types.ObjectId, ref: 'Like'}
+    }
+
+
+, { timestamps: true });
+
+const Notification = mongoose.model('Notification', notificationSchema);
+export default Notification;
